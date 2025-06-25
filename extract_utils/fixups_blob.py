LineageOS/android_tools_extract-utils

extract_utils: implement clear_symbol_version() blob fixup
Change-Id: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8

extract_utils/fixups_blob.py
@@ -135,6 +135,23 @@ class blob_fixup:
         impl = partial(self.remove_needed_impl, lib)
         return self.call(impl)
 
+    def clear_symbol_version_impl(
+        self,
+        symbol: str,
+        ctx: BlobFixupCtx,
+        file: File,
+        file_path: str,
+        *args,
+        **kwargs,
+    ):
+        run_cmd(
+            [self.__patchelf_path, '--clear-symbol-version', symbol, file_path]
+        )
+
+    def clear_symbol_version(self, lib: str) -> blob_fixup:
+        impl = partial(self.clear_symbol_version_impl, lib)
+        return self.call(impl)
+
     def fix_soname_impl(
         self, ctx: BlobFixupCtx, file: File, file_path: str, *args, **kwargs
     ):

yaap/tools_extract-utils

extract_utils/fixups_blob.py
@@ -135,6 +135,23 @@ class blob_fixup:
         impl = partial(self.remove_needed_impl, lib)
         return self.call(impl)
 
+    def clear_symbol_version_impl(
+        self,
+        symbol: str,
+        ctx: BlobFixupCtx,
+        file: File,
+        file_path: str,
+        *args,
+        **kwargs,
+    ):
+        run_cmd(
+            [self.__patchelf_path, '--clear-symbol-version', symbol, file_path]
+        )
+
+    def clear_symbol_version(self, symbol: str) -> blob_fixup:
+        impl = partial(self.clear_symbol_version_impl, symbol)
+        return self.call(impl)
+
     def fix_soname_impl(
         self, ctx: BlobFixupCtx, file: File, file_path: str, *args, **kwargs
     ):