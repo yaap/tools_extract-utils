LineageOS/android_tools_extract-utils

extract_utils: make a more sophisticated `call()` example
Change-Id: Ie545160fa14a0cfa09369fc7a86160ac70144157

templates/single-device-py/extract-files.py
@@ -19,6 +19,12 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
     'device/lineage/example',
@@ -40,16 +46,30 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 
-def blob_fixup_test_flag(
+def blob_fixup_return_1(
     ctx: BlobFixupCtx,
     file: File,
     file_path: str,
+    disassemble_symbol: str,
     *args,
     **kwargs,
 ):
-    with open(file_path, 'rb+') as f:
-        f.seek(1337)
-        f.write(b'\x01')
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {disassemble_symbol}'):
+            offset, _ = line.split(maxsplits=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
 
 
 blob_fixups: blob_fixups_user_type = {
@@ -69,7 +89,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('from.so', 'to.so')
         .binary_regex_replace(b'\xFF\x00\x00\x94', b'\xFE\x00\x00\x94')
         .sig_replace('C0 03 5F D6 ?? ?? ?? ?? C0 03 5F D6', '1F 20 03 D5')
-        .call(blob_fixup_test_flag),
+        .call(blob_fixup_return_1, 'license_check')
 }  # fmt: skip
 
 module = ExtractUtilsModule(

yaap/tools_extract-utils

templates/single-device-py/extract-files.py
@@ -19,6 +19,12 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
     'device/lineage/example',
@@ -40,16 +46,30 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 
-def blob_fixup_test_flag(
+def blob_fixup_return_1(
     ctx: BlobFixupCtx,
     file: File,
     file_path: str,
+    symbol: str,
     *args,
     **kwargs,
 ):
-    with open(file_path, 'rb+') as f:
-        f.seek(1337)
-        f.write(b'\x01')
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {symbol}'):
+            offset, _ = line.split(maxsplits=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
 
 
 blob_fixups: blob_fixups_user_type = {
@@ -69,7 +89,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('from.so', 'to.so')
         .binary_regex_replace(b'\xFF\x00\x00\x94', b'\xFE\x00\x00\x94')
         .sig_replace('C0 03 5F D6 ?? ?? ?? ?? C0 03 5F D6', '1F 20 03 D5')
-        .call(blob_fixup_test_flag),
+        .call(blob_fixup_return_1, 'license_check'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(