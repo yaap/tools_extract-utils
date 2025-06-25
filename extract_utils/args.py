LineageOS/android_tools_extract-utils

extract_utils: add support for selecting a module based on device name
Change-Id: Ie3c0b6e953f66040e81a3753db5fa7060840a33d

extract_utils/args.py
@@ -19,6 +19,10 @@ group.add_argument(
     help='Extract all files from archive',
 )
 group.add_argument(
+    '--only-name',
+    help='only extract module with device name',
+)
+group.add_argument(
     '--only-common',
     action='store_true',
     help='only extract common module',
@@ -94,6 +98,7 @@ class Args:
         self.extract_all: bool = args.extract_all
         self.only_common: bool = args.only_common
         self.only_target: bool = args.only_target
+        self.only: str = args.only
         self.extract_factory: bool = args.extract_factory
         self.regenerate_makefiles: bool = args.regenerate_makefiles
         self.regenerate: bool = args.regenerate

extract_utils/main.py
@@ -39,7 +39,12 @@ class ExtractUtils:
         self.__args = parse_args()
 
         self.__modules: List[ExtractUtilsModule] = []
-        if self.__args.only_target:
+        if self.__args.only:
+            all_modules = [device_module] + common_modules
+            for module in all_modules:
+                if module.device == self.__args.only:
+                    self.__modules.append(module)
+        elif self.__args.only_target:
             self.__modules.append(device_module)
         elif self.__args.only_common:
             assert common_modules is not None

yaap/tools_extract-utils

extract_utils/args.py
@@ -19,6 +19,10 @@ group.add_argument(
     help='Extract all files from archive',
 )
 group.add_argument(
+    '--only-name',
+    help='only extract module with device name',
+)
+group.add_argument(
     '--only-common',
     action='store_true',
     help='only extract common module',
@@ -94,6 +98,7 @@ class Args:
         self.extract_all: bool = args.extract_all
         self.only_common: bool = args.only_common
         self.only_target: bool = args.only_target
+        self.only_name: str = args.only_name
         self.extract_factory: bool = args.extract_factory
         self.regenerate_makefiles: bool = args.regenerate_makefiles
         self.regenerate: bool = args.regenerate

extract_utils/main.py
@@ -39,7 +39,12 @@ class ExtractUtils:
         self.__args = parse_args()
 
         self.__modules: List[ExtractUtilsModule] = []
-        if self.__args.only_target:
+        if self.__args.only_name:
+            all_modules = [device_module] + common_modules
+            for module in all_modules:
+                if module.device == self.__args.only_name:
+                    self.__modules.append(module)
+        elif self.__args.only_target:
             self.__modules.append(device_module)
         elif self.__args.only_common:
             self.__modules.extend(common_modules)